803480dc82354c2d056998709b87a333beee41fb
monicavladarean <mvladarean@yahoo.com>
2021-05-30 15:14:08 +0300

    about us page for adults mode, done

--- clientApp/src/app/company-details/company-details.component.ts
+++ clientApp/src/app/company-details/company-details.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyDetailsComponent implements OnInit {
 
-  constructor() { }
+  userType:string;
+
+  constructor() {
+    this.userType = sessionStorage.getItem('userType');
+   }
 
   ngOnInit(): void {
   }
